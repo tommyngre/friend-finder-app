299977fb43090cae93579de19af6230600386830
unknown <tommyn.gre@gmail.com>
2018-05-14 22:47:17 -0400

    gettin started

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
 let express = require('express');
 let bodyParser = require('body-parser');
 let path = require('path');
+
+let app = express();
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+
+let PORT = process.env.PORT || 3000;
+
+require("./routing/apiRoutes")(app);
+require("./routing/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
