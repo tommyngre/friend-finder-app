8ce8b2e1c31a46f06a35b5a1484ebfa850aa2c02
unknown <tommyn.gre@gmail.com>
2018-05-16 00:00:13 -0400

    + get routes

--- server.js
+++ server.js
@@ -8,8 +8,10 @@ app.use(bodyParser.json());
 
 let PORT = process.env.PORT || 3000;
 
-require("./routing/apiRoutes")(app);
-require("./routing/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+
 
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
